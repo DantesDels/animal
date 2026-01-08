79a510f13fe4e0936924d34fd377aabac16b2586
-c <-c>
2026-01-08 10:25:26 +0100

    feat : exercice 1

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public abstract class Animal {
+    protected String name;
+    protected int age;
+    protected String sound;
+
+    public void makeSound() {
+        System.out.println(sound);
+    }
+}
---
+++ src/Cow.java
@@ -0,0 +1,8 @@
+public class Cow extends Animal {
+
+    public Cow() {
+        this.name = "Cow";
+        this.age = 45;
+        this.sound = "Meuh";
+    }
+}
